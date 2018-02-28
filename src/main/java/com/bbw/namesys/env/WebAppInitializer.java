328ceedf9e11a71f05b9732549a38d5b36ccdc7a
chengli <326018662@qq.com>
2018-02-28 23:16:46 +0800

    '设置session过期时间为6小时'

--- src/main/java/com/bbw/namesys/env/WebAppInitializer.java
+++ src/main/java/com/bbw/namesys/env/WebAppInitializer.java
@@ -4,7 +4,10 @@ import com.bbw.namesys.base.AccessFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import java.util.EnumSet;
+import java.util.concurrent.TimeUnit;
 import javax.servlet.*;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
 
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -37,6 +40,16 @@ public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServlet
     public void onStartup(ServletContext servletContext) throws ServletException {
         servletContext.addFilter("AccessFilter", new AccessFilter()).
                 addMappingForServletNames(EnumSet.of(DispatcherType.REQUEST), true, this.getServletName());
+        servletContext.addListener(new HttpSessionListener() {
+            @Override
+            public void sessionCreated(HttpSessionEvent se) {
+                se.getSession().setMaxInactiveInterval((int) TimeUnit.HOURS.toSeconds(6));
+            }
+            @Override
+            public void sessionDestroyed(HttpSessionEvent se) {
+
+            }
+        });
         super.onStartup(servletContext);
     }
 }
